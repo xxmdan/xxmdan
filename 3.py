b64fd09d6b1dbe00b2f8915dc5017e8b03711696
xxmdan <50266127+xxmdan@users.noreply.github.com>
2019-05-16 01:10:53 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -3,6 +3,7 @@ def frame(words):
     size = len(max(words, key=len))
     print('*' * (size + 4))
     for word in words:
-        (print('* {a:<{b}} *'.format(a=word, b=size)),'* {word} *')
+        print('* {:<{}} *'.format(word, size))
     print('*' * (size + 4))
 frame(p.split(" "))
+
